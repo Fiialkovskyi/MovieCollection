09ffb567f1290879e8ae62316f992ba676b61769
Fiialkovskyi <rapgame1992@yahoo.com>
2020-08-12 16:41:54 +0300

    Добавил обработчик ошибок в API

--- source/js/searchMovies.js
+++ source/js/searchMovies.js
@@ -12,16 +12,28 @@ function getMovies(searchText) {
     .then(function (responce) {
       console.log(responce);
       var movies = responce.data.Search;
+      var error = responce.data.Error;
       var output = "";
-      $.each(movies, function (index, movie) {
-        output += `
+
+      if(error == "Too many results.") {
+        output = `
+         <p class="search-results__error">Too many results.</p>
+        `;
+      } else if (error == "Movie not found!") {
+        output = `
+         <p class="search-results__error">Movie not found!</p>
+        `;
+      } else {
+        $.each(movies, function (index, movie) {
+          output += `
           <div class="search-results__item">
             <img class="search-results__poster" src="${movie.Poster}">
             <h3 class="search-results__title">${movie.Title} (${movie.Type}, ${movie.Year})</h3>
             </div>
            </div>
         `;
-      });
+        });
+      }
 
       $("#searchResults").html(output);
     })
